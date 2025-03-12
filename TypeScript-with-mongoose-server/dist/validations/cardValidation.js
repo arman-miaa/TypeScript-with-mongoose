fe474a8f7a455573c62e99ee3eef647c0640d99b
Arman <mdarman365506@gmail.com>
2025-03-12 10:41:04 +0600

    some changed

--- TypeScript-with-mongoose-server/dist/validations/cardValidation.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const zod_1 = require("zod");
-const cardValidationSchema = zod_1.z.object({
-    title: zod_1.z.string().min(1, "Title is required"),
-    body: zod_1.z.string().optional(),
-    date: zod_1.z.date().optional(),
-});
-exports.default = cardValidationSchema;
